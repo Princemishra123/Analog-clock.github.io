d22097e1437bc4f63442bb98dc1bbf9e00ec36c7
Prince mishra <prince22.pm@gmail.com>
2021-07-21 11:26:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+function callFun() {
+    alert("Do you want to see the time?");
+}
+document.write(callFun());
+setInterval(() => {
+    d = new Date();
+    htime = d.getHours();
+    mtime = d.getMinutes();
+    stime = d.getSeconds();
+    hrotation = 30*htime + mtime/2;
+    mrotation = 6*mtime;
+    srotation = 6*stime;
+
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
+}, 1000);
